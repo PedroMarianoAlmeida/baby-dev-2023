2419506b36be5b6b3dda373efe3ac4618b98179e
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-09-17 14:42:13 -0300

    passing pages to inside src

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import ButtonTest from "../src/components/ButtonTest";
+import ButtonTest from "src/components/ButtonTest";
 import styles from "@styles/Home.module.scss";
 
 const Home: NextPage = () => {
